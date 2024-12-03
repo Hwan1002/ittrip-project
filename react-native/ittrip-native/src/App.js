13faf1f6ad9426b78a8b7eb5a2db8b6237a3047d
hyejin <hj12121414@naver.com>
2024-12-03 16:19:28 +0900

    241203 react native start

---
+++ react-native/ittrip-native/src/App.js
@@ -0,0 +1,31 @@
+
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react'
+import { View, StyleSheet, Text } from 'react-native'
+import Main from './screen/Main.js'
+import NewPlan from './screen/NewPlan.js'
+import PlanView from './screen/PlanView.js'
+import MyPage from './screen/MyPage.js'
+
+const Tab = createBottomTabNavigator()
+
+const App = () => {
+
+    return (
+        <NavigationContainer>
+            <Tab.Navigator initialRouteName='main'>
+                <Tab.Screen name='main' component={Main} />
+                <Tab.Screen name='newPlan' component={NewPlan} />
+                <Tab.Screen name="planView" component={PlanView} />
+                <Tab.Screen name="myPage" component={MyPage} />
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
+};
+
+const styles = StyleSheet.create({
+
+})
+
+export default App;
