e91945918fe423b94a7ec93fe300b934d691d589
hyejin <hj12121414@naver.com>
2024-12-04 09:47:33 +0900

    241204 리엑트네이티브 시작

--- react-native/ittrip-native/src/App.js
+++ react-native/ittrip-native/src/App.js
@@ -2,7 +2,7 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react'
-import { View, StyleSheet, Text } from 'react-native'
+import { View, StyleSheet, Text, Image } from 'react-native'
 import Main from './screen/Main.js'
 import NewPlan from './screen/NewPlan.js'
 import PlanView from './screen/PlanView.js'
@@ -10,11 +10,18 @@ import MyPage from './screen/MyPage.js'
 
 const Tab = createBottomTabNavigator()
 
+
 const App = () => {
 
     return (
         <NavigationContainer>
-            <Tab.Navigator initialRouteName='main'>
+            <Tab.Navigator 
+                initialRouteName='main'
+                screenOptions={{
+                    headerTitle:()=>(
+                        <Image source={require('./img/logo.svg')} style={{width:'50px'}}/>
+                )}}
+            >
                 <Tab.Screen name='main' component={Main} />
                 <Tab.Screen name='newPlan' component={NewPlan} />
                 <Tab.Screen name="planView" component={PlanView} />
