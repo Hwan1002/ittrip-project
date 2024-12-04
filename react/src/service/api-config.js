572963cf97121d265158d16fc1984e35fb9f7da4
Hwan1002 <ladudae112@gmail.com>
2024-12-04 09:15:36 +0900

    Update api-config.js

--- react/src/service/api-config.js
+++ react/src/service/api-config.js
@@ -3,6 +3,6 @@ let backendHost;
 const hostname = window && window.location && window.location.hostname;
 
 if(hostname === 'localhost'){
-    backendHost = "http://localhost:8080";
+    backendHost = "http://localhost:9090";
 }
 export const API_BASE_URL = `${backendHost}`;
