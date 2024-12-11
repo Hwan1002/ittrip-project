479fbb64046fab5f2e1043a4de6c85e032de643d
gusdyd0102 <gusdyd0102@gmail.com>
2024-12-11 15:18:04 +0900

    찐찐막

--- spring/work/map/src/main/java/project/map/controller/TripController.java
+++ spring/work/map/src/main/java/project/map/controller/TripController.java
@@ -70,8 +70,8 @@ public class TripController {
 	//TripEntity 객체들 반환(여행목록에 여행리스트)
 	//만약 @RequestParam으로 쓰면 (@RequestParam String userId)
 	@GetMapping("/3")
-	public ResponseEntity<?> getTrips(@RequestBody UserDTO dto){
-		List<TripEntity> list = tripService.getTrips(dto.getId());
+	public ResponseEntity<?> getTrips(@RequestParam String userId){
+		List<TripEntity> list = tripService.getTrips(userId);
 		List<TripDTO> dtos = list.stream().map(TripDTO::new).toList();
 		ResponseDTO<TripDTO> response = ResponseDTO.<TripDTO>builder().data(dtos).build();
 		return ResponseEntity.ok(response);
@@ -79,8 +79,8 @@ public class TripController {
 	//MapEntity 객체들 반환(userId와 title을 통해서 map객체(days,start정보 등등 받아옴))
 	//만약 @RequestParam으로 쓰면 (@RequestParam String userId,@RequestParam String title)
 	@GetMapping("/4")
-	public ResponseEntity<?> getMaps(@RequestBody TripDTO dto){
-		List<MapEntity> list = tripService.getMaps(dto.getUserId(), dto.getTitle());
+	public ResponseEntity<?> getMaps(@RequestParam String userId,@RequestParam String tripTitle){
+		List<MapEntity> list = tripService.getMaps(userId, tripTitle);
 		List<MapDTO> dtos = list.stream().map(MapDTO::new).toList();
 		ResponseDTO<MapDTO> response = ResponseDTO.<MapDTO>builder().data(dtos).build();
 		return ResponseEntity.ok(response);
@@ -88,8 +88,8 @@ public class TripController {
 	//CheckListEntity 객체 반환(userId와 title을 통해서 checkList객체(checkList) 받아옴)
 	//만약 @RequestParam으로 쓰면 (@RequestParam String userId, String title)
 	@GetMapping("/5")						
-	public ResponseEntity<?> getCheckList(@RequestBody CheckListDTO dto){
-		CheckListEntity entity = tripService.getCheckLists(dto.getUserId(), dto.getTripTitle());
+	public ResponseEntity<?> getCheckList(@RequestParam String userId,@RequestParam String tripTitle){
+		CheckListEntity entity = tripService.getCheckLists(userId, tripTitle);
 		String[] response = tripService.stringToMap(entity.getCheckList());
 		return ResponseEntity.ok(response);
 	}
