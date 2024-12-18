975b6fd743f9da6da37b8a33609b275965a0faad
gusdyd0102 <gusdyd0102@gmail.com>
2024-12-18 09:00:10 +0900

    my b

--- react/src/components/Map.js
+++ react/src/components/Map.js
@@ -1,11 +1,63 @@
-import React, { useEffect, useContext } from "react";
+import React, { useEffect, useContext, useState } from "react";
 import { ProjectContext } from "../context/ProjectContext";
 
 const Map = () => {
 
-  // 임시 배열
-  const dayChacks = ['일정1', '일정2', '일정3'];
-  const { address, path, startPoint, setStartPoint, goalPoint, setGoalPoint, setWaypoints,wayPoints, markers,setMarkers } = useContext(ProjectContext);
+  const { tripDates, address, path, startPoint, setStartPoint, goalPoint, setGoalPoint, wayPoints, setWaypoints,
+    startPlace,startAddress,goalPlace,goalAddress,wayPointsPlace,wayPointsAddress} = useContext(ProjectContext);
+
+    // days: selectedDay.substring(3,4),
+    // startPlace: startPlace,
+    // startAddress: startAddress,
+    // goalPlace: goalPlace,
+    // goalAddress: goalAddress,
+    // wayPointsPlace: wayPointsPlace,
+    // wayPointsAddress: wayPointsAddress,
+
+  const [dayChecks, setDayChecks] = useState([])
+  const [selectedDay, setSelectedDay] = useState(null);  // 선택된 날짜를 저장할 상태
+  const [dayBoolean,setDayBoolean] = useState([]);
+  const [dayData, setDayData] = useState([])
+
+  //day 눌렀을 때 axios로 경로데이터 받아오는 거랑 day마다 selectedDay dayChecks day있으면 없으면 따로따로 동작 로직구현
+  //day를 옮길 때 기본으로 dayData에 값을 넘겨야함(만약 dayData에 selectedDay의 day가 있으면 해당 day의 객체 덮어씌우기)
+  //만약 selectedDay의 day가 dayData에 존재하는 day인 경우 해당 day에 맞는 day객체를 불러와서 경로상태에 set을 해야함
+
+  if(dayBoolean[parseInt(selectedDay.substring(3,4))-1]){       //선택된 Day가 Day3이라면 dayBoolean[2] 가 true라면 =>(해당 Day에 객체를 넣었다면)
+    const foundData = dayData.find(data => data.days == selectedDay.substring(3,4));    //  선택한 day에 대한 객체를 가져옴
+    //foundData는 눌렀던 Day에 해당하는 map객체가 들어있으니 객체의 내부 값으로 경로 등의 상태를 set해주세요.
+  }else if(!(dayBoolean[parseInt(selectedDay.substring(3,4))-1])){      //dayBoolean[2] 가 false라면 =>(해당 Day에 객체가 없다면)
+    // 상태들과 map을 초기화 해주세요.
+  }      
+
+  const 객체넣기 = (selectedDay) => {         //Day를 옮길 때(selectedDay 값이 바뀌기 전에 작동) 
+    const updatedDayBoolean = dayBoolean;
+    updatedDayBoolean[parseInt(selectedDay.substring(3,4))-1] = true;
+    setDayBoolean(updatedDayBoolean);
+    setDayData([...dayData,{
+      // days: ~~, startPlace: ~~ 등등 객체 넣기
+    }])
+  }
+
+  useEffect(() => {
+    if (tripDates && tripDates.startDate && tripDates.endDate) {
+      const startDate = new Date(tripDates.startDate);
+      const endDate = new Date(tripDates.endDate);
+
+      // 출발일자와 도착일자 간의 차이를 계산 (밀리초 단위)
+      const diffTime = endDate - startDate;
+
+      // 차이를 일수로 변환
+      const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)) + 1; // +1일은 시작일도 포함
+
+      // dayChecks 배열 업데이트
+      const daysArray = Array.from({ length: diffDays }, (_, index) => `Day${index + 1}`);
+      setDayChecks(daysArray);
+
+      setDayBoolean(new Boolean[dayChecks.length]);
+    }
+  }, [tripDates]);
+
 
   useEffect(() => {
     // Naver 지도 API 스크립트 로드
@@ -20,6 +72,11 @@ const Map = () => {
           zoom: 15
         });
 
+
+        // LatLngBounds 객체 생성: 모든 마커를 포함할 범위 계산
+        const bounds = new window.naver.maps.LatLngBounds();
+
+
         if (address) {
           window.naver.maps.Service.geocode({
             query: address
@@ -32,44 +89,78 @@ const Map = () => {
             // 변환된 좌표 가져오기
             const result = response.v2;
             const latlng = new window.naver.maps.LatLng(result.addresses[0].y, result.addresses[0].x);
-            // console.log(latlng)
 
-            // 첫 번째 좌표 저장
+
             if (!startPoint) {
               setStartPoint(latlng)
-             const newMarker = new window.naver.maps.Marker({
-                position: latlng,
-                map: map,
-                title: "출발지"
-              });
-              setMarkers(prev => [...prev,newMarker]);
+
             } else if (startPoint && !goalPoint) {
               setGoalPoint(latlng)
-             const newMarker =  new window.naver.maps.Marker({
-                position: latlng,
-                map: map,
-                title: "목적지"
-              });
-              setMarkers(prev => [...prev,newMarker]);
-            } 
-            // else if (startPoint && goalPoint ) {
-            //   setWaypoints(prevWaypoints => [...prevWaypoints, latlng]);
-            //  const newMarker =  new window.naver.maps.Marker({
-            //     position: latlng,
-            //     map: map,
-            //     title: `경유지${wayPoints.length+1}`
-            //   });
-            //   setMarkers(prev => [...prev,newMarker]);
-            // }
-            
+            } else if (!wayPoints.some(point => point.equals(latlng)) && !goalPoint.equals(latlng)) {
+              setWaypoints(prevWaypoints => [...prevWaypoints, latlng]);
+            }
 
             // 지도 위치를 마커로 이동
             map.setCenter(latlng);
 
 
-            // 두 번째 좌표가 설정되면 폴리라인을 그리기
+            // 마커를 모두 추가하기
+            const markers = [];
+
+            //마커모양 교체
+            const createMarkerIcon = (text) => {
+              return {
+                content: `
+                    <div style="width: 30px; height: 30px; background-color: white; color: black; text-align: center; border-radius: 50%; line-height: 30px; font-size: 14px; font-weight: bold; position: relative; border: 3px solid #F6A354;">
+                      ${text}
+                      <div style="content: ''; position: absolute; bottom: -10px; left: 50%; transform: translateX(-50%); width: 0; height: 0; border-left: 10px solid transparent; border-right: 10px solid transparent; border-top: 10px solid #F6A354;"></div>
+                    </div>
+                  `,
+                size: new window.naver.maps.Size(30, 30),
+                anchor: new window.naver.maps.Point(15, 15),
+              };
+            };
+
+
+
+
+            // 시작 지점 마커 추가
+            if (startPoint) {
+              const startMarker = new window.naver.maps.Marker({
+                position: startPoint,
+                map: map,
+                icon: createMarkerIcon('S')
+              });
+              markers.push(startMarker);
+              bounds.extend(startMarker.getPosition()); // 시작 지점 위치 추가
+            }
+
+            // 목표 지점 마커 추가
+            if (goalPoint) {
+              const goalMarker = new window.naver.maps.Marker({
+                position: goalPoint,
+                map: map,
+                icon: createMarkerIcon('G')
+              });
+              markers.push(goalMarker);
+              bounds.extend(goalMarker.getPosition()); // 목표 지점 위치 추가
+            }
+
+            // 경유지 마커 추가
+            if (wayPoints && wayPoints.length > 0) {
+              wayPoints.forEach((point, index) => {
+                const waypointMarker = new window.naver.maps.Marker({
+                  position: point,
+                  map: map,
+                  icon: createMarkerIcon(`${index + 1}`)
+                });
+                markers.push(waypointMarker);
+                bounds.extend(waypointMarker.getPosition()); // 경유지 위치 추가
+              });
+            }
+
+            // 경로 표시하기 
             if (path) {
-              console.log(path)
               const pathCoordinates = path.map(([longitude, latitude]) => new window.naver.maps.LatLng(latitude, longitude))
 
 
@@ -81,8 +172,11 @@ const Map = () => {
               });
 
               polyline.setMap(map);  // 지도에 폴리라인을 표시
+              pathCoordinates.forEach(coord => bounds.extend(coord));// 경로 좌표도 범위에 추가
             }
-
+            // 모든 마커와 경로를 포함하는 범위로 지도를 자동으로 조정
+            map.fitBounds(bounds)
+            map.setZoom(map.getZoom() - 1); // 한 단계 더 줌 아웃해서 여유를 줍니다.
           });
         }
       }
@@ -95,6 +189,12 @@ const Map = () => {
       document.body.removeChild(script);
     };
   }, [address, path]);
+
+  // Day 클릭 시, 해당 날짜에 맞는 지도 업데이트
+  const handleDayClick = (day) => {
+    setSelectedDay(day);
+    // selectedDay를 기반으로 지도에 표시할 경로 또는 다른 로직 추가
+  };
 
 
   return (
@@ -121,8 +221,8 @@ const Map = () => {
         top: '10px',   // 부모 div의 위에서 20px 떨어지게 설정
         left: "14px",
       }}>
-        {/* dayChacks 배열의 항목에 따라 DayN 요소 생성 */}
-        {dayChacks.map((day, index) => (
+        {/* dayChecks 배열의 항목에 따라 DayN 요소 생성 */}
+        {dayChecks.map((index) => (
           <div style={{
             textAlign: "center",
             width: 62,   // 각 Day 요소의 너비
@@ -132,7 +232,11 @@ const Map = () => {
             borderRadius: 18,
             padding: "4px 6px 2px 4px",
             margin: "20px 10px", // 요소들 사이의 여백
-          }} key={index}>Day {index + 1}</div>
+          }} key={index}
+            onClick={() => handleDayClick(index)}
+          >
+            {index}
+          </div>
         ))}
       </div>
     </div>
